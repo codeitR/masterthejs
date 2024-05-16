eea0d2f178a5534d50b2032a1e871dbf233852d7
codeitR <90155745+codeitR@users.noreply.github.com>
2024-05-16 13:49:05 +0200

    weatherapp

---
+++ weather-app/script.js
@@ -0,0 +1,8 @@
+const apikey = "81ab8f6c37d5ec92866bc49ab4db65f0";
+
+const weatherDataEl = document.getElementById("weather-data");
+const cityInputEl = document.getElementById("city-input");
+
+const formEl = document.querySelector("form");
+
+formEl.addEventListener("submit", () => {});
