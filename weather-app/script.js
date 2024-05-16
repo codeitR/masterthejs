e6e790f652c5efb2395e62be62da79e33d91f247
codeitR <90155745+codeitR@users.noreply.github.com>
2024-05-16 16:14:24 +0200

    weatherapi

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,8 +1,43 @@
-const apikey = "81ab8f6c37d5ec92866bc49ab4db65f0";
+const apiKey = "290c8ec9b81f3160b9f3991bd5748164";
 
 const weatherDataEl = document.getElementById("weather-data");
 const cityInputEl = document.getElementById("city-input");
 
 const formEl = document.querySelector("form");
 
-formEl.addEventListener("submit", () => {});
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const cityValue = cityInputEl.value;
+  getWeatherData(cityValue);
+});
+async function getWeatherData(cityValue) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${apiKey}&q=${cityValue}&units=metric`
+    );
+
+    if (!response.ok) {
+      throw new Error("City is not available at the moment");
+    }
+    const data = await response.json();
+
+    const temperature = Math.round(data.main.temp);
+    const description = data.weather[0].description;
+    const icon = data.weather[0].icon;
+    const details = [
+      `Feels like: ${Math.round(data.main.feels_like)} °C`,
+      `Humidity:${data.main.humidity} %`,
+      `Wind speed:${data.wind.speed} m/s`,
+    ];
+    weatherDataEl.querySelector(".icon").innerHTML = `<img
+    src="http://openweathermap.org/img/wn/${icon}.png"
+    alt="weather/sun icon" />`;
+    weatherDataEl.querySelector(
+      ".temperature"
+    ).textContent = `${temperature}°C`;
+    weatherDataEl.querySelector(".description").textContent = description;
+    weatherDataEl.querySelector(".details").innerHTML = details
+      .map((detail) => `<div>${detail}</div>`)
+      .join("");
+  } catch (error) {}
+}
